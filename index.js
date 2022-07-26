c2855aea04fdc88e49e037d87fd74d14bdf83844
huangminguang <15896938825@163.com>
2022-07-26 17:46:15 +0800

    feat: 父级创建index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: TerryMin
+ * @Date: 2022-07-26 17:45:00
+ * @LastEditors: TerryMin
+ * @LastEditTime: 2022-07-26 17:45:23
+ * @Description: file not
+ */
+console.log('main branch');
